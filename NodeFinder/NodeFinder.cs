f89b2c63e0c5bfbd780250e869ee161752340453
jieaido <jieaido@163.com>
2016-03-29 23:01:49 +0800

    哎呀我操又更新了一下算法

--- NodeFinder/NodeFinder.cs
+++ NodeFinder/NodeFinder.cs
@@ -8,6 +8,9 @@ public class PathFinder
     public List<PathNode> Nodes = new List<PathNode>();
 
     public List<PathNode> OpenNodes = new List<PathNode>();
+    private PathNode _startNode;
+    private PathNode _goalNode;
+
 
     public void InitPath(int xNum, int yNum, int rockNum)
     {
@@ -32,36 +35,77 @@ public class PathFinder
 
     public bool SerachGoal(Postion startPostion, Postion goalpostion)
     {
-        
-        List<PathNode> CalcNodes=new List<PathNode>();
+
+        List<PathNode> CalcNodes;//这是寻找到的
        //todo   增加positon类的==操作符重写，判断startpostion==goalposition的情况 
-        var startNode = Nodes.Find(p => p.Position == startPostion );
-        var goalNode = Nodes.Find(p => p.Position == goalpostion);
-        if (startNode==null||goalNode==null)
+        _startNode = Nodes.Find(p => p.Position == startPostion );
+        _goalNode = Nodes.Find(p => p.Position == goalpostion);
+        if (_startNode==null||_goalNode==null)
         {
             return false;
         }
-        startNode.StartG = 0;
-        startNode.EndH = PathNode.CalcEndH(startPostion, goalpostion);
-        startNode.ParentNode = null;
-        OpenNodes.Add(startNode);
+        _startNode.StartG = 0;
+        _startNode.EndH = PathNode.CalcEndH(startPostion, goalpostion);
+        _startNode.ParentNode = null;
+        OpenNodes.Add(_startNode);
+        PathNode calcNode;//这是需要计算的点,就是计算这个点的周围
         while (OpenNodes.Count>0)
         {
-           var Calc= OpenNodes.Min(s => s.TotalF);
-            FindGroundNodes();
-        }
+            calcNode = OpenNodes.OrderBy(s=>s.TotalF).First();
+            if (calcNode!=_goalNode)
+            {
+                FindGroundNodes(calcNode, out CalcNodes);
+                for (int i = 0; i < CloseNodes.Count; i++)
+                {
+                    if (OpenNodes.Contains(CalcNodes[i]))
+                    {
 
+                    }
+                }
+               
+            }
+           
+        }
 
 
         return false;
     }
 
-    private void FindGroundNodes(PathNode pn )
+    private void FindGroundNodes(PathNode pn,out List<PathNode> pns )
     {
 
         var p1=  Nodes.Find(p=>p.Position.X==pn.Position.X&&p.Position.Y==p.Position.Y+1);
         var p2=  Nodes.Find(p=>p.Position.X==pn.Position.X&&p.Position.Y==p.Position.Y-1);
         var p3=  Nodes.Find(p=>p.Position.X==pn.Position.X+1&&p.Position.Y==p.Position.Y);
         var p4=  Nodes.Find(p=>p.Position.X==pn.Position.X-1&&p.Position.Y==p.Position.Y);
+        PathNode[] pp=new PathNode[]
+        {
+            p1,p2,p3,p4
+        };
+       
+        pns=new List<PathNode>();
+        for (int i = 0; i < pp.Length; i++)
+        {
+            if (pp[i] != null && pp[i].IsPass == true)
+            {
+                pp[i].ParentNode = pn;
+                CalcTotalF(pp[i],_goalNode);
+                pns.Add(pp[i]);
+            }
+        }
+       
+       
+       
+
+    }
+
+    private void CalcTotalF(PathNode thisNode, PathNode goalNode)
+    {
+        if (thisNode.ParentNode!=null)
+        {
+            thisNode.StartG = thisNode.ParentNode.StartG + PathNode.CalcEndH(thisNode.Position, thisNode.ParentNode.Position);
+        }
+        thisNode.EndH = PathNode.CalcEndH(thisNode.Position, goalNode.Position);
+
     }
 }
